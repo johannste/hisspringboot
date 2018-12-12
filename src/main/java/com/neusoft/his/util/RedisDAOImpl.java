21da4c2acf9637620f3913e063e2d7da4f5876e6
johannste <saint.sola@yahoo.com>
2018-12-12 23:55:41 +0800

    made some optimizations

---
+++ src/main/java/com/neusoft/his/util/RedisDAOImpl.java
@@ -0,0 +1,13 @@
+package com.neusoft.his.util;
+
+public class RedisDAOImpl implements RedisDAO {
+    @Override
+    public String get(String key) {
+        return null;
+    }
+
+    @Override
+    public long incr(String key) {
+        return 0;
+    }
+}
--- src/main/java/com/neusoft/his/util/SerialNumberServiceImpl.java
+++ src/main/java/com/neusoft/his/util/SerialNumberServiceImpl.java
@@ -7,22 +7,23 @@ import javax.xml.rpc.ServiceException;
 
 @Service
 public class SerialNumberServiceImpl implements SerialNumberService {
+
     @Resource
-    private RedisDAO redisDao;
+    private RedisDAO redisDAO;
 
     @Override
     public String generate(String bizCode) throws ServiceException {
-/** 检查业务码 */
+/* 检查业务码 */
         boolean isLegal = isLegal(bizCode);
         if (!isLegal) {
             throw new ServiceException("bizCode参数不合法");
         }
-/** 获取今天的日期:yyyyMMdd */
+/* 获取今天的日期:yyyyMMdd */
         String date = TimeUtil.getToday();
-/** 构造redis的key */
+/* 构造redis的key */
         String key = SERIAL_NUMBER + date;
-/** 自增 */
-        long sequence = redisDao.incr(key);
+/* 自增 */
+        long sequence = redisDAO.incr(key);
         String seq = StringUtil.getSequence(sequence);
         StringBuilder sb = new StringBuilder();
         sb.append(date).append(bizCode).append(seq);
