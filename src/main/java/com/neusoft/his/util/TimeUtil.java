5524833c9276de87aa250d863d9e646a01a2f85d
johannste <saint.sola@yahoo.com>
2018-12-12 23:34:47 +0800

    made some optimizations

--- src/main/java/com/neusoft/his/util/TimeUtil.java
+++ src/main/java/com/neusoft/his/util/TimeUtil.java
@@ -1,17 +1,11 @@
 package com.neusoft.his.util;
 
 import java.util.Calendar;
-import java.util.Date;
 
 public class TimeUtil {
     private TimeUtil() {
     }
 
-    /**
-     * 获取今日日期
-     *
-     * @return
-     */
     public static String getToday() {
         Calendar now = Calendar.getInstance();
         return now.get(Calendar.YEAR) + "" + now.get(Calendar.MONTH) + now.get(Calendar.DATE);
