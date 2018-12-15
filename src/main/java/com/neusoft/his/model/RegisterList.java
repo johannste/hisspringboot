6bd38ab382340b099b046c719a12eca82e8b307a
zhouzy <saint.sola@yahoo.com>
2018-12-15 12:54:18 +0800

    add query patient model

---
+++ src/main/java/com/neusoft/his/model/RegisterList.java
@@ -0,0 +1,85 @@
+package com.neusoft.his.model;
+
+public class RegisterList {
+    private Integer id;
+    private String name;
+    private String identifyType;
+    private String departmentName;
+    private String doctor;
+    private String date;
+    private String time;
+    private String registerNumber;
+    private Integer expenses;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdentifyType() {
+        return identifyType;
+    }
+
+    public void setIdentifyType(String identifyType) {
+        this.identifyType = identifyType;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(String doctor) {
+        this.doctor = doctor;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getRegisterNumber() {
+        return registerNumber;
+    }
+
+    public void setRegisterNumber(String registerNumber) {
+        this.registerNumber = registerNumber;
+    }
+
+    public Integer getExpenses() {
+        return expenses;
+    }
+
+    public void setExpenses(Integer expenses) {
+        this.expenses = expenses;
+    }
+}
