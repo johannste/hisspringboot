542e6aa329b637da385d3ebb1ce55da046ccfed4
johannste <saint.sola@yahoo.com>
2018-12-18 02:21:20 +0800

    add log4j2

---
+++ src/main/java/com/neusoft/his/model/PatientList.java
@@ -0,0 +1,157 @@
+package com.neusoft.his.model;
+
+public class PatientList {
+    private Integer id;
+    private String name;
+    private String gender;
+    private Integer age;
+    private String provence;
+    private String city;
+    private String moreAdress;
+    private String registerType;
+    private String registerNumber;
+    private String phone;
+    private String relatedName;
+    private String relatedPhoneNumber;
+    private String relatedType;
+    private String symptoms;
+    private String illHistory;
+    private String regionType;
+    private String createDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getProvence() {
+        return provence;
+    }
+
+    public void setProvence(String provence) {
+        this.provence = provence;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getMoreAdress() {
+        return moreAdress;
+    }
+
+    public void setMoreAdress(String moreAdress) {
+        this.moreAdress = moreAdress;
+    }
+
+    public String getRegisterType() {
+        return registerType;
+    }
+
+    public void setRegisterType(String registerType) {
+        this.registerType = registerType;
+    }
+
+    public String getRegisterNumber() {
+        return registerNumber;
+    }
+
+    public void setRegisterNumber(String registerNumber) {
+        this.registerNumber = registerNumber;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getRelatedName() {
+        return relatedName;
+    }
+
+    public void setRelatedName(String relatedName) {
+        this.relatedName = relatedName;
+    }
+
+    public String getRelatedPhoneNumber() {
+        return relatedPhoneNumber;
+    }
+
+    public void setRelatedPhoneNumber(String relatedPhoneNumber) {
+        this.relatedPhoneNumber = relatedPhoneNumber;
+    }
+
+    public String getRelatedType() {
+        return relatedType;
+    }
+
+    public void setRelatedType(String relatedType) {
+        this.relatedType = relatedType;
+    }
+
+    public String getSymptoms() {
+        return symptoms;
+    }
+
+    public void setSymptoms(String symptoms) {
+        this.symptoms = symptoms;
+    }
+
+    public String getIllHistory() {
+        return illHistory;
+    }
+
+    public void setIllHistory(String illHistory) {
+        this.illHistory = illHistory;
+    }
+
+    public String getRegionType() {
+        return regionType;
+    }
+
+    public void setRegionType(String regionType) {
+        this.regionType = regionType;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+}
