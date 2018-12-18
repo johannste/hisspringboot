a3ed48a2976575a11bdc007b589175896b0247a2
johannste <saint.sola@yahoo.com>
2018-12-18 23:54:47 +0800

    add echarts to show register truly in one week group by department

--- src/main/java/com/neusoft/his/mapper/RegistrationMapper.java
+++ src/main/java/com/neusoft/his/mapper/RegistrationMapper.java
@@ -8,6 +8,6 @@ import java.util.Map;
 
 public interface RegistrationMapper {
 
-    @Select("SELECT count(*) AS `value`,dept_name AS `name` FROM register_manager rm LEFT JOIN department d ON rm.department_id=d.id GROUP BY dept_name;")
+    @Select("SELECT count(*) AS `value`,dept_name AS `name` FROM register_manager rm LEFT JOIN department d ON rm.department_id=d.id WHERE YEARWEEK(DATE_FORMAT(register_date,'%Y-%m-%d'))=YEARWEEK(NOW()) GROUP BY dept_name;")
     List<Map<String, RegisterGroupByDepartment>> getRegisterGroupByDepartment();
 }
