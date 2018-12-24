8625bffaefdfbe8d7350710c56f3bb128500468e
zhouzy <saint.sola@yahoo.com>
2018-12-24 09:27:24 +0800

    add echarts model

---
+++ src/main/java/com/neusoft/his/model/RegisterGroupByDepartment.java
@@ -0,0 +1,23 @@
+package com.neusoft.his.model;
+
+public class RegisterGroupByDepartment {
+
+    private Integer value;
+    private String name;
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
